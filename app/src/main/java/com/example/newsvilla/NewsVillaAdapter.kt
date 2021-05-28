d97a5906f68d4767b31bb2673580a6be837e31ed
AkshayAnil1080 <akshayanil356@gmail.com>
2021-05-28 12:23:04 +0530

    added recycler view and layout manager

---
+++ app/src/main/java/com/example/newsvilla/NewsVillaAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.newsvilla
+
+import android.view.View
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsVillaAdapter: RecyclerView.Adapter<>{
+    // have to make it adapter of Recycler view ie. to extend(:) the class
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+}
